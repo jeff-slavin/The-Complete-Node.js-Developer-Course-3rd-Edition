6bf691b306291ee5d2c6e44a14b5093f81ac93b0
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-13 21:49:22 -0600

    Importing npm modules lesson complete

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,6 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
+console.log(validator.isEmail('jeff.slavin@outlook.com'));
+console.log(validator.isURL('https://hangman.slavsites.com'));
