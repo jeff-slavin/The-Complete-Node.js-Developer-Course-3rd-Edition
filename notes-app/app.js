dce514d9ec363b4af5b848e192cb2b0388d3ba38
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-13 23:15:18 -0600

    Argument parsing with yargs part 1 lesson complete

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,62 @@
 //const validator = require('validator');
-const getNotes = require('./notes.js');
+const yargs = require('yargs');
 const chalk = require('chalk');
+const getNotes = require('./notes.js');
 
-const command = process.argv[2];
 
-console.log(process.argv);
+//const command = process.argv[2];
+//console.log(process.argv);
 
-if (command === 'add') {
-    console.log('Adding note!');
-} else if (command === 'remove') {
-    console.log('Removing note!');
-};
+// Customize Yargs Version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note');
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+});
+
+// Listing out the notes
+yargs.command({
+    command: 'list',
+    describe: 'Listing out the notes',
+    handler: function () {
+        console.log('Listing out the notes');
+    }
+});
+
+// Reading a note
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+});
+
+
+
+
+
+console.log(yargs.argv);
+
+// if (command === 'add') {
+//     console.log('Adding note!');
+// } else if (command === 'remove') {
+//     console.log('Removing note!');
+// };
 
 // console.log(getNotes());
 // console.log(validator.isEmail('jeff.slavin@outlook.com'));
