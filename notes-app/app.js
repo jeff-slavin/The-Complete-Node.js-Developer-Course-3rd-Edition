c213e69378655d703a7a3e64c9f2a2a4559fbd05
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-13 22:13:09 -0600

    Printing in color lesson complete

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,12 @@
-const validator = require('validator');
+//const validator = require('validator');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
 console.log(getNotes());
-console.log(validator.isEmail('jeff.slavin@outlook.com'));
-console.log(validator.isURL('https://hangman.slavsites.com'));
+//console.log(validator.isEmail('jeff.slavin@outlook.com'));
+//console.log(validator.isURL('https://hangman.slavsites.com'));
+
+const chalkError = chalk.bold.red;
+
+console.log(chalk.green.bold.inverse('Success!'));
+console.log(chalkError('This is my error message!'));
