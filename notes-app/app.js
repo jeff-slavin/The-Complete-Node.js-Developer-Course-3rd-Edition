3c0dc15d12379ea8911cd01f45d0a54bad51e773
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-13 22:20:57 -0600

    Global npm modules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,7 +6,7 @@ console.log(getNotes());
 //console.log(validator.isEmail('jeff.slavin@outlook.com'));
 //console.log(validator.isURL('https://hangman.slavsites.com'));
 
-const chalkError = chalk.bold.red;
+const chalkError = chalk.bold.yellow;
 
-console.log(chalk.green.bold.inverse('Success!'));
+console.log(chalk.blue.bold.inverse('Success!'));
 console.log(chalkError('This is my error message!'));
