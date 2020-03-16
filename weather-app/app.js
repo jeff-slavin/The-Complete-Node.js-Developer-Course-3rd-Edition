b7038c28118445bc1423a1f53232669bd851cdd9
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-15 22:54:43 -0600

    Making http requests lesson complete - dark sky

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,24 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000);
+const url = 'https://api.darksky.net/forecast/6006c02deeed9fafd051c34da1805c7f/37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0);
+request({ url: url}, (error, response) => {
+    //console.log(error);
+    const data = JSON.parse(response.body); // parse the data
+    console.log(data.currently);
+    //console.log(response);
+});
 
-console.log('Stopping');
+
+
+// console.log('Starting');
+
+// setTimeout(() => {
+//     console.log('2 second timer');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('0 second timer');
+// }, 0);
+
+// console.log('Stopping');
