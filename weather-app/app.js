3bcae1613d86028e38710fdadc10da3daec63f1b
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-16 22:34:05 -0600

    Callback abstraction challenge lesson complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,29 @@
-const request = require('request');
+//const request = require('request');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 geocode('Long Beach', (error, data) => {
     console.log('Error: ', error);
     console.log('Data: ', data);
 });
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
+});
+
+
+
 
 // const url = 'https://api.darksky.net/forecast/6006c02deeed9fafd051c34da1805c7f/37.8267,-122.4233?';
 // // invalid URL for error handling
