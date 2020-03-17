763b3df2df6d07bbb5002beedc978f20b301ed66
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-16 23:53:17 -0600

    Destructuring and property shorthand challenge lesson complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,19 +11,19 @@ if(!address) {
 
 } else {
 
-    geocode(address, (error, geocodeData) => {
+    geocode(address, (error, { latitude, longitude, location }) => {
     
         if (error) {
             return console.log('Error: ' + error);
         };
     
-        forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
     
             if (error) {
                 return console.log('Error: ' + error);
             };
     
-            console.log(geocodeData.location);
+            console.log(location);
             console.log(forecastData);
             
         });
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (latitude, longitude, callback) => {
     //const url = `https://api.darksky.net/forecast/6006c02deeed9fafd051c34da1805c7f/${latitude},${longitude}`;
     const url = 'https://api.darksky.net/forecast/6006c02deeed9fafd051c34da1805c7f/' + latitude + ',' + longitude;
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error) {
             callback('Unable to connect to weather service.', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location.', undefined);
         } else {
-            callback(undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
         };
     });
 };
