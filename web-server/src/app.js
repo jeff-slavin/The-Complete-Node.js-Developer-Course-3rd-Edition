e5b75d98024ac0cee3065267e1385db5acc99e42
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-18 14:18:56 -0600

    Serving up HTML and JSON lesson complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,28 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello express!');
+    res.send('<H1>Weather</H1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help Page');
+    res.send([{
+        name: 'Jeff',
+        age: 38
+    }, {
+        name: 'Jenn',
+        age: 37
+    }]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.send('<H1>About</H1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page');
+    res.send({
+        location: 'Highlands Ranch',
+        forecast: 'It is really nice outside!'
+    });
 });
 
 app.listen(3000, () => {
