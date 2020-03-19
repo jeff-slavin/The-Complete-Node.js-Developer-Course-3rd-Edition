747d9792e70e8259a1c46e19c8368c611faee5f7
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-18 22:09:39 -0600

    Serving up static assets lesson complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,13 @@
+const path = require('path');
 const express = require('express');
 
+// console.log(__dirname);
+// console.log(path.join(__dirname, '../public'));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('<H1>Weather</H1>');
-});
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Jeff',
-        age: 38
-    }, {
-        name: 'Jenn',
-        age: 37
-    }]);
-});
-
-app.get('/about', (req, res) => {
-    res.send('<H1>About</H1>');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -30,3 +19,22 @@ app.get('/weather', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
 });
+
+// app.get('', (req, res) => {
+//     res.send('<H1>Weather</H1>');
+// });
+
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Jeff',
+//         age: 38
+//     }, {
+//         name: 'Jenn',
+//         age: 37
+//     }]);
+// });
+
+// app.get('/about', (req, res) => {
+//     res.send('<H1>About</H1>');
+// });
+
