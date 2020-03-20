a469b80140bfa1ab0ded7055d95c8351762c30c8
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-19 21:56:57 -0600

    The query string lesson complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -41,9 +41,29 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        });
+    };
+    
     res.send({
         location: 'Highlands Ranch',
-        forecast: 'It is really nice outside!'
+        forecast: 'It is really nice outside!',
+        address: req.query.address
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term.'
+        });
+    };
+    console.log(req.query.search);
+    res.send({
+        products: []
     });
 });
 
