e3aeb3ccc3d6920f77d8912f4a520a173d664d21
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-19 22:19:35 -0600

    Building a json http endpoint lesson complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 const app = express();
 
 // Define paths for express config
@@ -47,11 +50,23 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address.'
         });
     };
-    
-    res.send({
-        location: 'Highlands Ranch',
-        forecast: 'It is really nice outside!',
-        address: req.query.address
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error });
+        };
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            };
+
+            res.send({
+                location,
+                forecast: forecastData,
+                address: req.query.address
+            });
+        });
     });
 });
 
