195d27276ef27f72ca6550a316fa56d6d2ebe706
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-19 23:04:15 -0600

    Creating a search form lesson complete

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,21 @@
 console.log('Client side javascript file is loaded.');
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        };
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = searchElement.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            };
+        });
     });
 });
