ab4647cccbb0c41dda64bd5439557c9f9a1a0c55
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-20 23:47:07 -0600

    New feature deployment workflow lesson complete

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${Math.round(body.currently.temperature)} degrees out. The high today is ${Math.round(body.daily.data[0].temperatureHigh)} with a low of ${Math.round(body.daily.data[0].temperatureLow)}. There is a ${Math.round(body.currently.precipProbability)}% chance of rain.`);
         };
     });
 };
