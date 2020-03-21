6fd12fac3f4eedab91a00f8d01872a84e88a5e9f
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-20 23:01:47 -0600

    Deploying node.js to heroku - making it environment agnostic (e.g. ports & /weather call)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -102,7 +103,7 @@ app.get('*', (req, res) => {
 });
 
 // Start server at port 3000
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000.');
 });
 
