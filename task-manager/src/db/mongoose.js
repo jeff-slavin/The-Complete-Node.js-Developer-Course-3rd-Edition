59870f33056fe6c1cd10f8fd1ce1ee4caadcaada
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-26 22:22:13 -0600

    Data validation and sanitization part 1 and 2 lessons complete

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true,
@@ -7,36 +8,69 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            };
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"');
+            };
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            };
+        }
     }
 });
 
 // const me = new User({
-//     name: 'Jeff',
-//     age: 'Mike'
+//     name: '   Jeff    ',
+//     email: 'MYEMAIL@slavin.IO    ',
+//     password: 'phone098!'
 // });
 
 // me.save().then(() => {
 //     console.log(me);
 // }).catch((error) => {
 //     console.log('Error:', error);
-// });
+// })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
 const task = new Task({
-    description: 'Learn the Mongoose library',
-    completed: false
+    description: '   Eat lunch'
 });
 
 task.save().then(() => {
