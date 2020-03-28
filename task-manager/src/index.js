7f47cfcf0c1d2f47c027153a25f9e9f19b4ffd7b
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-27 21:38:51 -0600

    Resource reading endpoints part 2 lesson complete

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -36,7 +36,7 @@ app.get('/users/:id', (req, res) => {
     User.findById(_id).then((user) => {
         if (!user) {
             return res.status(404).send();
-        }
+        };
         res.send(user);
     }).catch((error) => {
         res.status(500).send();
@@ -50,6 +50,27 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task);
     }).catch ((error) => {
         res.status(400).send(error);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send();
+        };
+        res.send(task);
+    }).catch((error) => {
+        res.status(500).send();
     });
 });
 
