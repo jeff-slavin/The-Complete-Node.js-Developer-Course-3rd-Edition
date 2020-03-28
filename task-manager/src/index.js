7b90a4cffb52c20e326a6142a53a2233a835d888
Jeff Slavin <jeff.slavin@outlook.com>
2020-03-27 21:03:20 -0600

    Resource creation endpoints part 2 lesson complete

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,11 @@
+// To start up mongo server
+// /Users/"Jeff Slavin"/mongodb/bin/mongod.exe --dbpath=/Users/"Jeff Slavin"/mongodb-data
+
 const express = require('express');
 require('./db/mongoose');
 
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +16,17 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
+    }).catch ((error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
     }).catch ((error) => {
         res.status(400).send(error);
     });
