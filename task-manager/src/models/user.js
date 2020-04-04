0de74ae59e9efcd52433b68c8e2eb2cb4567c240
Jeff Slavin <jeff.slavin@outlook.com>
2020-04-04 00:29:09 -0600

    Working with timestamps lesson complete

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -48,6 +48,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 });
 
 // Virtual Property - a relationship between two entities
