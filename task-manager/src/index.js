71f6b277ae04904e08433b8a8f622b299bf6e87c
Jeff Slavin <jeff.slavin@outlook.com>
2020-04-04 23:38:36 -0600

    Auto-cropping and image formatting lesson complete

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,34 +4,8 @@ const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
-
 const app = express();
 const port = process.env.PORT || 3000;
-
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word document'));    // reject upload, throw error back
-        };
-
-        cb(undefined, true);       // allow upload 
-        
-        // cb(new Error('File must be a PDF'));    // sending back an error
-        // cb(undefined, true);    // success and upload is expected
-        // cb(undefined, false);   // silently rejects the upload (not really used)
-    }
-});
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send();
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-});
 
 app.use(express.json());
 app.use(userRouter);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const router = new express.Router();
 const auth = require('../middleware/auth');
 const User = require('../models/user');
@@ -99,7 +100,8 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
@@ -110,6 +112,22 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined;
     await req.user.save();
     res.send();
-})
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        };
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+
+    } catch (error) {
+        res.status(404).send();
+    };
+});
 
 module.exports = router;
