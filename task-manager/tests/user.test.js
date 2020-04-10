d0b6307de3a82508ec41aef609a4c3cd1fae7056
Jeff Slavin <jeff.slavin@outlook.com>
2020-04-09 23:06:50 -0600

    Wrapping up user tests lesson complete

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -101,3 +101,37 @@ test('Should not delete account for unauthenticated user', async () => {
         .send()
         .expect(401);
 });
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));    // checks that user.avatar is a buffer (not looking at actual value)
+});
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Larry'
+        })
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toEqual('Larry');
+});
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Denver'
+        })
+        .expect(400);
+});
