e89299bed1ac418ab9eab7dbcd9268745e99d609
Jeff Slavin <jeff.slavin@outlook.com>
2020-04-11 22:25:06 -0600

    Socket.io events challenge lesson complete

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,17 +12,13 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
     console.log('New web socket connection');
 
-    socket.emit('countUpdated', count);
+    socket.emit('message', 'Welcome!'); // send only to the specific client
 
-    socket.on('increment', () => {
-        count++;
-        //socket.emit('countUpdated', count);   // emits to just the specific client connection
-        io.emit('countUpdated', count);         // emits to all connections
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);    // send to all connected clients
     });
 });
 
