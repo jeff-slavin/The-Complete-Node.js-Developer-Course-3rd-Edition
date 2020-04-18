0a66e1a21329e6738388e2bc94783489db78eb70
Jeff Slavin <jeff.slavin@outlook.com>
2020-04-17 22:18:20 -0600

    Rendering location messages lesson complete

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -9,11 +9,20 @@ const $messages = document.querySelector('#messages');
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
 socket.on('message', (msg) => {
-    console.log(msg);
+    //console.log(msg);
     const html = Mustache.render(messageTemplate, {
         message: msg
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', (url) => {
+    //console.log(url);
+    const html = Mustache.render(locationMessageTemplate, {
+        url
     });
     $messages.insertAdjacentHTML('beforeend', html);
 });
